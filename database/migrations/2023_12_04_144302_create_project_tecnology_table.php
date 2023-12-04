0f88aaab0ac30389d3d1e68339fab007009bb267
Melozzo <carmecamma1995@gmail.com>
2023-12-04 15:53:08 +0100

    [create]: table for manyToMany

---
+++ database/migrations/2023_12_04_144302_create_project_tecnology_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('project_tecnology', function (Blueprint $table) {
+            $table->insignedBigInteger('project_id');
+            $table->foreign('project_id')
+                ->references('id')
+                ->on('project')
+                ->cascadeOnDelete();
+        });
+
+        Schema::create('project_tecnology', function (Blueprint $table) {
+            $table->insignedBigInteger('tecnology_id');
+            $table->foreign('tecnology_id')
+                ->references('id')
+                ->on('tecnology')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project_tecnology');
+    }
+};
